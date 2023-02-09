78fdfb656137a4871986c21d170620879c0bccad
sautmanurung1 <smanurung360@gmail.com>
2023-02-10 05:16:20 +0700

    update about project routes

--- src/routes/project.js
+++ src/routes/project.js
@@ -3,8 +3,7 @@ const { body } = require('express-validator')
 const ProjectsControllers = require('../controllers/projects')
 const router = express.Router()
 
-router.post('/post', [body('title').isLength({ min: 5 }).withMessage('input title tidak sesuai'),
-body('description').isLength({ min: 5 }).withMessage('input description tidak sesuai')], ProjectsControllers.CreateProjectsControllers)
-
+router.post('/post', [body('title').isLength({ min: 5 }).withMessage('input title tidak sesuai'),body('description').isLength({ min: 5 }).withMessage('input description tidak sesuai')], ProjectsControllers.CreateProjectsControllers)
 router.get('/posts', ProjectsControllers.getAllProjectControllers)
+router.get('/post/:ProjectId', ProjectsControllers.getDataProjectById)
 module.exports = router
